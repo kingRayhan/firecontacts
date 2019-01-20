a357c3fe32691e1e3b3170c006cf060be842b4cc
Shoaib <shoaib19j@gmail.com>
2019-01-20 00:46:49 -0800

    private router

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'toastr/build/toastr.css'
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import db from './store/firebase'
-import Home from './pages/Home'
-import Add from './pages/Add'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Bookmarks from './pages/Bookmarks'
-import PrivateRoute from './components/PrivateRoute'
-import store from './store'
-import { authSetuser } from './store/reducers/authReducer'
-
-store.dispatch(authSetuser()) // check is logged in
+import React from 'react';
+import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'toastr/build/toastr.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { authSetuser } from './store/reducers/authReducer';
+import App from './App';
+store.dispatch(authSetuser()); // check is logged in
 
 render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/add" component={Add} />
-                <Route exact path="/bookmarks" component={Bookmarks} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.querySelector('#root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
